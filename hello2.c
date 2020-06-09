0f35c17de60e9c9913aacf54f51dcbc414ab5b30
wprzystu <59979223+wprzystu@users.noreply.github.com>
2020-06-09 10:23:54 -0500

    Update hello2.c

--- hello2.c
+++ hello2.c
@@ -11,10 +11,12 @@
 
 int main(int argc, char **argv) {
   printF("Ahmed Albalushi"):
-  printf("William przystup");
   printf("electrical engineering:);
   
   pirntf("Jake King");
+  printf("Mechanical Engineer);
+         
+  printf("William przystup");
   printf("Mechanical Engineer);
   
   return 0;
