909eec0f3b14ed756f204b21f747e26996838254
aalbalushi7 <66686041+aalbalushi7@users.noreply.github.com>
2020-06-09 10:12:43 -0500

    Create hello2.c

---
+++ hello2.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ahmed Albalushi
+ *         aalbalushi@cse.unl.edu
+ * Date: 2020/06/09
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
